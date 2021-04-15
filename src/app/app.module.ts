41e40a46fb80c0727fa94e268857ae471e840667
TEDSPOWERHOUSE\Trent <tdecakrd2000@yahoo.com>
2021-04-14 18:35:34 -0600

    Install ng Materials & cdk + stylize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { MeterDropdownItemComponent } from './meter-dropdown-item/meter-dropdown
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/meter-dropdown-item/meter-dropdown-item.component.ts
+++ src/app/meter-dropdown-item/meter-dropdown-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatAccordion} from '@angular/material/expansion';
 
 @Component({
   selector: 'app-meter-dropdown-item',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meter-dropdown-item.component.css']
 })
 export class MeterDropdownItemComponent implements OnInit {
+  @ViewChild(MatAccordion, { static: true }) accordion: MatAccordion = new MatAccordion;
+
+  dropdownOpen = false;
+  panelOpenState = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onMeterItemClick(){
+    this.dropdownOpen = this.dropdownOpen === false ? true : false;
+    console.log('dropdown clicked: ' + this.dropdownOpen)
   }
 
 }
