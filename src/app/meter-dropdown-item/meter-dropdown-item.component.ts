2316409ee9d273e1835fabd94f1230f6a1712116
TEDSPOWERHOUSE\Trent <tdecakrd2000@yahoo.com>
2021-04-15 18:02:58 -0600

    ngFor through meters in object + more styling

--- src/app/meter-dropdown-item/meter-dropdown-item.component.ts
+++ src/app/meter-dropdown-item/meter-dropdown-item.component.ts
@@ -7,17 +7,74 @@ import { Component, OnInit} from '@angular/core';
 })
 
 export class MeterDropdownItemComponent implements OnInit {
-  dropdownOpen = false;
+  dropdownOpen: {[k:number]:boolean} = {};
   panelOpenState = true;
+
+// All meter data stored here
+  meterData = [
+    {
+      manufacturer: 'ABB'
+    },
+    {
+      manufacturer: 'Actaris'
+    },
+    {
+      manufacturer: 'AMCO'
+    },
+    {
+      manufacturer: 'Assured Automation'
+    },
+    {
+      manufacturer: 'B Meters'
+    },
+    {
+      manufacturer: 'Badger'
+    },
+    {
+      manufacturer: 'Blue-White'
+    },
+    {
+      manufacturer: 'Byram Labs'
+    },
+    {
+      manufacturer: 'Carlon'
+    },
+    {
+      manufacturer: 'Conservice'
+    },
+    {
+      manufacturer: 'DLJ'
+    },
+    {
+      manufacturer: 'Elster'
+    },
+    {
+      manufacturer: 'GWF'
+    },
+    {
+      manufacturer: 'Hersey'
+    },
+    {
+      manufacturer: 'Inovonics'
+    },
+    {
+      manufacturer: 'Invensys'
+    },
+    {
+      manufacturer: 'Ista'
+    }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onMeterItemClick(){
-    this.dropdownOpen = this.dropdownOpen === false ? true : false;
-    console.log('dropdown clicked: ' + this.dropdownOpen)
+  onMeterItemClick(i:any){
+    //hide meter manufacturer underline when tile is open
+    //note: removing element from DOM causes page to 'jump'
+    //so set opacity to 0
+    this.dropdownOpen[i] = this.dropdownOpen[i] === true ? false : true;
   }
 
 }
