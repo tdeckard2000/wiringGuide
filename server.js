4f1b047b14e33f3b0e4d2d892922e1f28fe12c4f
Trent Deckard <tdeckard2000@yahoo.com>
2021-04-29 19:39:01 -0600

    Fix object compatibility html references

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ require('dotenv').config();
 // ************************************
 //            DB Settings
 // ************************************
+
 const uri = process.env.DB_URL;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -26,220 +27,34 @@ client.connect(err => {
   app.get('/api/allMeters', async(req, res)=>{
 
     result = await meterGuideData.find().toArray();
-    result.sort();
+    result.sort(sortMeterManufacturers);
     res.json(result);
   })
 
   if(err){console.log(err)}else{console.log('Connected to DB')};
-
-  // client.close();
 });
 
 // ************************************
-//            DB Functions
+//             Functions
 // ************************************
+const sortMeterManufacturers = function(meterA, meterB){
+
+  if(meterA.manufacturer < meterB.manufacturer){
+    return -1;
+
+  }else if(meterA.manufacturer > meterB.manufacturer){
+    return 1;
+
+  }else{
+    return 0
+  }
+};
+
 
 // ************************************
-//             API Routes
+//              Server
 // ************************************
-
-app.get('/', (req, res)=>{
-  res.send('<p>Hello World</p>')
-});
-
-// app.get('/api/allMeters', async(req, res)=>{
-
-//   res.json(meterData)
-// });
 
 app.listen(3000, (req, res)=>{
   console.log('Listening on Port 3000');
 });
-
-
-meterData = [
-  {
-    manufacturer: 'ABB',
-    sections: [
-      {
-        seriesName: 'Series Name Example',
-        seriesModel: 'C700 & InsideR',
-        meters: [
-          {
-            meterName: 'Inovonics/ABB Integrated',
-            signalType: 'integrated',
-            wiringProtocol: 'integrated',
-            compatibleTR201: false,
-            compatibleTR4: false,
-            compatibleRR4: false,
-            notes: 'This is a third party meter.'
-          },
-          {
-            meterName: 'Pulse Register',
-            signalType: 'pulse',
-            wiringProtocol: 'pulse',
-            compatibleTR201: true,
-            compatibleTR4: true,
-            compatibleRR4: true,
-            notes: 'This is an example support team note. Be sure that your wire is connected to the meter.'
-          },
-          {
-            meterName: 'Scancoder',
-            signalType: 'encoded',
-            wiringProtocol: 'sensus',
-            compatibleTR201: false,
-            compatibleTR4: true,
-            compatibleRR4: true,
-            notes: ''
-          }
-        ]
-      }]
-  },
-  {
-    manufacturer: 'Actaris',
-    sections: [
-      {
-        seriesName: '',
-        seriesModel: '',
-        meters: [
-          {
-            meterName: 'MultiMag',
-            signalType: 'pulse',
-            wiringProtocol: 'pulse',
-            compatibleTR201: true,
-            compatibleTR4: true,
-            compatibleRR4: true,
-            notes: ''
-          }
-        ]
-      }]
-  },
-  {
-    manufacturer: 'AMCO',
-    sections: [
-      {
-        seriesName: '',
-        seriesModel: 'C700, C400, V100 & Compound',
-        meters: [
-          {
-            meterName: 'Digital',
-            signalType: 'pulse',
-            wiringProtocol: 'pulse',
-            compatibleTR201: true,
-            compatibleTR4: true,
-            compatibleRR4: true,
-            notes: ''
-          },
-          {
-            meterName: 'InVISION',
-            signalType: 'encoded',
-            wiringProtocol: 'sensus',
-            compatibleTR201: false,
-            compatibleTR4: true,
-            compatibleRR4: true,
-            notes: ''
-          },
-          {
-            meterName: 'RS Pulser',
-            signalType: 'pulse',
-            wiringProtocol: 'pulse',
-            compatibleTR201: true,
-            compatibleTR4: true,
-            compatibleRR4: true
-          },
-          {
-            meterName: 'Scancoder',
-            signalType: 'encoded',
-            wiringProtocol: 'sensus',
-            compatibleTR201: false,
-            compatibleTR4: true,
-            compatibleRR4: true
-          },
-          {
-            meterName: 'InsideR (Inovonics/AMCO Integrated)',
-            signalType: 'integrated',
-            wiringProtocol: 'integrated',
-            compatibleTR201: false,
-            compatibleTR4: false,
-            compatibleRR4: false
-          }
-        ]
-      },
-      {
-        seriesName: '',
-        seriesModel: 'Misc',
-        meters: [
-          {
-            meterName: 'BK-G 1.6',
-            signalType: 'pulse',
-            wiringProtocol: 'pulse',
-            compatibleTR201: true,
-            compatibleTR4: true,
-            compatibleRR4: true,
-            notes: ''
-          },
-          {
-            meterName: 'G200',
-            signalType: 'pulse',
-            wiringProtocol: 'pulse',
-            compatibleTR201: true,
-            compatibleTR4: true,
-            compatibleRR4: true,
-            notes: ''
-          },
-          {
-            meterName: 'G4 200',
-            signalType: 'pulse',
-            wiringProtocol: 'pulse',
-            compatibleTR201: true,
-            compatibleTR4: true,
-            compatibleRR4: true,
-            notes: ''
-          }
-        ]
-      }
-     ]
-    },
-  {
-    manufacturer: 'Assured Automation'
-  },
-  {
-    manufacturer: 'B Meters'
-  },
-  {
-    manufacturer: 'Badger'
-  },
-  {
-    manufacturer: 'Blue-White'
-  },
-  {
-    manufacturer: 'Byram Labs'
-  },
-  {
-    manufacturer: 'Carlon'
-  },
-  {
-    manufacturer: 'Conservice'
-  },
-  {
-    manufacturer: 'DLJ'
-  },
-  {
-    manufacturer: 'Elster'
-  },
-  {
-    manufacturer: 'GWF'
-  },
-  {
-    manufacturer: 'Hersey'
-  },
-  {
-    manufacturer: 'Inovonics'
-  },
-  {
-    manufacturer: 'Invensys'
-  },
-  {
-    manufacturer: 'Ista'
-  }
-];
