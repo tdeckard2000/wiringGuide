7cf7985487190cacbd10425a3d7bad97930c78de
Trent Deckard <tdeckard2000@yahoo.com>
2021-04-30 18:26:14 -0600

    Begin building edit page

---
+++ src/app/edit-page/edit-page-title/edit-page-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-page-title',
+  templateUrl: './edit-page-title.component.html',
+  styleUrls: ['./edit-page-title.component.css']
+})
+export class EditPageTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
