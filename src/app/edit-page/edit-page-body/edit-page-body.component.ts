beb80957136f13cb13317acc29dff8b8369248d9
Trent <tdeckard2000@yahoo.com>
2021-05-10 22:32:47 -0600

    Work on logic for Add a Meter Manufacturere on edit page.

--- src/app/edit-page/edit-page-body/edit-page-body.component.ts
+++ src/app/edit-page/edit-page-body/edit-page-body.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OverlayContainer } from '@angular/cdk/overlay';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-edit-page-body',
@@ -7,8 +8,9 @@ import { OverlayContainer } from '@angular/cdk/overlay';
   styleUrls: ['./edit-page-body.component.css']
 })
 export class EditPageBodyComponent implements OnInit {
+  canAddNewSection = true;
   newSectionsCount = [];
-  newSectionValues : Object[] = [];
+  newSectionValues: [{seriesName:string, modelsName:string }] = [{seriesName: "", modelsName: ""}];
   radioSelection = "";
   radioOptions= [
     "Add a Meter",
@@ -31,17 +33,21 @@ export class EditPageBodyComponent implements OnInit {
 
   onReturnHome(){
     this.visibleTile = 'Home';
-  }
+  };
 
   onNewSection(){
     this.newSectionsCount.length ++;
+    this.canAddNewSection = false;
+    this.newSectionValues[this.newSectionsCount.length - 1] = ({"seriesName":"", "modelsName":""});
   };
 
   onRemoveSection(){
     this.newSectionsCount.length --;
-  }
+    this.canAddNewSection = true;
+    this.newSectionValues.pop();
+  };
 
-  //Store Series and Model Names Together
+  //Store Series and Model Names Together (Add a Meter Manufacturer)
   onSeriesName(seriesName:string, modelsName:string, index:number){
     this.newSectionValues[index] =
       {
@@ -49,16 +55,34 @@ export class EditPageBodyComponent implements OnInit {
         "modelsName": modelsName
       };
 
-    // Track if at least one of the two newest Series/Model fields has a value
-    // if(this.newSectionValues[index].seriesName)
-    console.log(this.newSectionValues)
-  }
+      console.log(this.newSectionValues)
+
+    //Can't add new section unless series or model name is given
+    for(let section of this.newSectionValues){
+      if(section.seriesName.length < 1 && section.modelsName.length < 1){
+        this.canAddNewSection = false;
+        break
+      }else{
+        this.canAddNewSection = true;
+      };
+    };
+
+    // if(seriesName.length > 0 || modelsName.length > 0){
+    //   this.canAddNewSection = true;
+    // }else{
+    //   this.canAddNewSection = false;
+    // };
+  };
 
   constructor(overlayContainer: OverlayContainer) {
     overlayContainer.getContainerElement().classList.add('angularTheme');
   };
 
   ngOnInit(): void {
+    interface SeriesModelNames{
+      seriesName: string,
+      modelsName: string
+    };
   };
 
 }
