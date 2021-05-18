6a82c1e9ef4f16ab533199e8e5a15eaf5883ef67
Trent <tdeckard2000@yahoo.com>
2021-05-18 17:56:32 -0600

    Fix searchbar bug

--- src/app/filter-meters.pipe.ts
+++ src/app/filter-meters.pipe.ts
@@ -1,56 +1,60 @@
+import { ScrollStrategyOptions } from '@angular/cdk/overlay';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'filterMeters'
 })
-export class FilterMetersPipe implements PipeTransform {  
+export class FilterMetersPipe implements PipeTransform {
   transform(meterData:Array<object> , searchBarText: string) {
     let filteredMeterData:any;
     //Begin filtering only if 3 characters are entered
     if(searchBarText && searchBarText.length >= 3){
       searchBarText = searchBarText.toLowerCase();
       filteredMeterData = meterData.filter((meter: any) => {
-        
+
         //Include meter if manufacturer name matches
         let manufacturer = meter.manufacturer.toLowerCase();
         if(manufacturer.includes(searchBarText)){
           return true;
         };
-        
+
         //Include meter if seriesName, seriesModel, or meterName matches
         let keepMeter = false;
-        
+
         //Check Series Name
         if(meter.sections){
           meter.sections.forEach((section:any)=>{
             let seriesName = section.seriesName.toLowerCase();
             if(seriesName.includes(searchBarText)){
               keepMeter = true;
-              return
             };
           });
         };
+
         //Check Series Model
         if(meter.sections){
           meter.sections.forEach((section:any)=>{
-            let seriesModel = section.seriesModel.toLowerCase();
-            if(seriesModel.includes(searchBarText)){
+            let modelsName = section.modelsName.toLowerCase();
+            if(modelsName.includes(searchBarText)){
               keepMeter = true;
             };
           });
         };
+
         //Check Individual Meter Names
         if(meter.sections){
           meter.sections.forEach((section:any) => {
-            console.dir(section)
+            if(!section.meters){return}
             section.meters.forEach((meter:any)=>{
-              let meterName = meter.meterName.toLowerCase();
+              let meterName = meter.meterName;
+              if(!meterName){return};
+              meterName = meter.meterName.toLowerCase();
               if(meterName.includes(searchBarText)){
                 keepMeter = true;
               };
             })
           });
-        }
+        };
         return keepMeter;
       });
 
