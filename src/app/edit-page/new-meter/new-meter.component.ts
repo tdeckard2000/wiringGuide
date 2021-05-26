93eb6a84f09161768e60234e9ef25ccbb82948af
Trent <tdeckard2000@yahoo.com>
2021-05-25 21:02:50 -0600

    begin building new meter tool

--- src/app/edit-page/new-meter/new-meter.component.ts
+++ src/app/edit-page/new-meter/new-meter.component.ts
@@ -1,5 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { EditPageService } from '../edit-page.service';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { MainService } from 'src/app/main.service';
+import { map, startWith } from 'rxjs/operators';
+
+interface NewMeterForm {
+  manufacturerUtilityType: string,
+  manufacturerName: string,
+  seriesName: string,
+  modelsName: string,
+  meterName: string,
+  signalType: string,
+  wiringProtocol: string,
+  compatibleWithTR201: boolean,
+  compatibleWithTR4: boolean,
+  compatibleWithRR4: boolean,
+  notes?: string
+}
 
 @Component({
   selector: 'app-new-meter',
@@ -8,10 +27,50 @@ import { EditPageService } from '../edit-page.service';
 })
 export class NewMeterComponent implements OnInit {
 
-  constructor(private editPageService: EditPageService) { }
+  constructor(private editPageService: EditPageService, public mainService: MainService, public dialog: MatDialog) { }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.manufacturerNames.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+  };
+
+  canClickSave = false;
+  filteredDropdownOptions: Observable<string[]> | undefined;
+  manufacturerNames = [""];
+  selectedManufacturerName = new FormControl
+  utilityTypeOptions = this.editPageService.utilityTypeOptions;
+  utilityTypeSelection = "";
+
+  onClickSave(){
+
+  };
 
   onReturnHome(){
     this.editPageService.visibleTile$.next('Home');
+  };
+
+  onUtilityType(data:{value: string}){
+    this.utilityTypeSelection = data.value;
+    this.setManufacturerList();
+  };
+
+  setManufacturerList(){
+    //define array for manufacturer drop down list
+    this.mainService.getArrayOfManufacturersByUtility(this.utilityTypeSelection)
+    .subscribe((data:object)=>{
+      this.manufacturerNames = data as Array<string>;
+      this.updateManufacturerDropdownList();
+    });
+    //clear input field
+    this.selectedManufacturerName.setValue("");
+  };
+
+  updateManufacturerDropdownList(){
+    //update manufacturer dropdown list with new options
+    this.filteredDropdownOptions = this.selectedManufacturerName.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
   };
 
   ngOnInit(): void {
