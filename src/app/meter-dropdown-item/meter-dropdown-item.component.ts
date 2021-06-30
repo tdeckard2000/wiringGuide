fef824b0c5f117a27176c64c126e4379f0bc22e2
Trent <tdeckard2000@yahoo.com>
2021-06-30 17:33:03 -0600

    Lighten manufacturer HRs and fix vanishing HR bug

--- src/app/meter-dropdown-item/meter-dropdown-item.component.ts
+++ src/app/meter-dropdown-item/meter-dropdown-item.component.ts
@@ -11,28 +11,23 @@ import { Observable } from 'rxjs';
 })
 
 export class MeterDropdownItemComponent implements OnInit {
-  dropdownOpen: {[k:number]:boolean} = {};
+  dropdownOpen:Array<boolean> = [];
   panelOpenState = true;
   searchBarText = '';
   meterData:Array<object> = [];
 
   constructor(private mainService : MainService,  public dialog: MatDialog){}
 
-  // All meter data
-
-  ngOnInit(): void {
-    this.mainService.searchBarText$.subscribe(data =>{
-      this.searchBarText = data;
-    });
-
-    this.mainService.getAllMeters().subscribe((res:any)=>{
-      this.meterData = res;
-    });
-  }
+  closeAllDropdowns(){
+    for(let i = 0; i<this.dropdownOpen.length; i++){
+      this.dropdownOpen[i] = false;
+    };
+  };
 
   //Hide meter manufacturer underline while tile is open
   onMeterItemClick(i:any){
     this.dropdownOpen[i] = this.dropdownOpen[i] === true ? false : true;
+    console.log(this.dropdownOpen)
   }
 
   //Open Wiring Guide Modal
@@ -46,4 +41,23 @@ export class MeterDropdownItemComponent implements OnInit {
       console.warn('Dialog result:' + result);
     });
   }
+
+  populateDropdownOpenArray(meterData:Array<object>){
+    meterData.forEach((itemInArray:object)=>{
+      this.dropdownOpen.push(false)
+    });
+  }
+
+  ngOnInit(): void {
+    this.mainService.searchBarText$.subscribe(data =>{
+      this.searchBarText = data;
+      this.closeAllDropdowns();
+    });
+
+    this.mainService.getAllMeters().subscribe((res:any)=>{
+      this.meterData = res;
+      this.populateDropdownOpenArray(this.meterData);
+    });
+  };
+
 }
